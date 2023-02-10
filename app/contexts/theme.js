8b73b47987add8f90a71c5cce0a7dad0dca8dd78
saugusth <54689875+saugusth@users.noreply.github.com>
2023-02-10 04:16:24 -0600

    battle

--- app/contexts/theme.js
+++ app/contexts/theme.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const { Consumer, Provider } = React.createContext()
+const ThemeContex = React.createContext()
 
-export const ThemeConsumer = Consumer
-export const ThemeProvider = Provider
+export default ThemeContex
+export const ThemeConsumer = ThemeContex.Consumer
+export const ThemeProvider = ThemeContex.Provider
